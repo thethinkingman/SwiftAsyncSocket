6f0253f7bd8b6eb671351c6ac90a5d8ef6983dd6
Jason Rooth <jason.rooth@thethinkingman.com.au>
2024-09-01 01:33:25 +1000

    Updated convert to UInt16

--- Source/SwiftAsyncSocket/SwiftAsyncSocketExtensions/Utilities/SwiftAsyncSocket+General.swift
+++ Source/SwiftAsyncSocket/SwiftAsyncSocketExtensions/Utilities/SwiftAsyncSocket+General.swift
@@ -10,10 +10,6 @@ import Foundation
 // MARK: - General Functions
 extension SwiftAsyncSocket {
 
-	// Method to safely convert an Int to UInt16
-	static func convertToUInt16(port: Int) -> UInt16 {
-	    // Convert Int to UInt16 with default value (e.g., 0) if conversion fails
-	    return UInt16(exactly: port) ?? 0
-	}
+
 
 }
